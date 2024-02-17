643653f5f799640582e021b490799985cf16eddb
mohamed37sad <simo.sad04@gmail.com>
2024-02-17 12:52:25 +0100

    simo

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,35 +1,25 @@
 #!/usr/bin/python3
+
 """
-Module Docs
+lists all states from the database hbtn_0e_0_usa
 """
-from sys import argv
+
 import MySQLdb
+from sys import argv
 
 
-def main():
-    """
-    Function DOC
-    """
-    db_user = argv[1]
-    db_password = argv[2]
-    db_db = argv[3]
-    db_host = "localhost"
-    db_port = 3306
-
-    try:
-        conn = MySQLdb.connect(host=db_host, port=db_port,
-                               user=db_user, passwd=db_password,
-                               db=db_db, charset="utf8")
-        cur = conn.cursor()
-        cur.execute("SELECT id, name FROM states ORDER BY id ASC")
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-        cur.close()
-        conn.close()
-    except Exception:
-        pass
+def get_states(username, password, dbname):
+    """lists all states from the database hbtn_0e_0_usa"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=str(username),
+                         passwd=str(password), db=str(dbname), charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    get_states(argv[1], argv[2], argv[3])
